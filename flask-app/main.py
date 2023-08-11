ee74b5e4152a55d7dd9bd53d6c5fd83d3aeef3e2
Arjan van der Laan <laanvanderarjan@gmail.com>
2023-08-11 23:03:08 +0000

    Change flask-app/main.py

--- flask-app/main.py
+++ flask-app/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # Assign a function to be called when the path `/` is requested
 @app.route("/")
 def index():
-    return "Hello, worldyyyyyyyyyyyyyyyiiiiioooooo!"
+    return "Hello, worldyyyyyyyyyyyyyyyyiiiiioooooo!"
 
 
 @app.route("/cow")
