8ab6ea42642f07d5fe414fcef16e3b183f870675
Arjan <laanvanderarjan@gmail.com>
2023-08-12 16:40:13 +0000

    yes

--- flask-app/main.py
+++ flask-app/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # Assign a function to be called when the path `/` is requested
 @app.route("/")
 def index():
-    return "Hellooooo0000oooioueeeeeeeeeeaaaaaaaaouoiuooowww, worldyyyyyyyyyyyyyyyyiiiiioooooo!"
+    return "Helloooo0000oooioueeeeeeeeeeaaaaaaaaouoiuooowww, worldyyyyyyyyyyyyyyyyiiiiioooooo!"
 
 
 @app.route("/cow")
